c3a54ecf38dacd7d573c0c376169b38a273c918f
shubila <shubi@hotmail.com>
2021-04-11 17:46:03 +0200

    Feature importance plot 2

--- scripts/generate_ml_objects/generate_ml_featimp_objects_full.R
+++ scripts/generate_ml_objects/generate_ml_featimp_objects_full.R
@@ -5,6 +5,7 @@ library("iml")
 library("future")
 library("future.callr")
 library("ggplot2")
+library("Metrics")
 
 #clear the global environment
 rm(list = ls(all.names = TRUE))
@@ -17,30 +18,32 @@ load("./output/ml_object/caret/full/test_train_full_data.RData")
 
 #  create matrices for generating the featimp  scores
 
-X <- lgbti_partial_train[!names(lgbti_partial_train) %in% c("open_at_work_bin")]
+X <- lgbti_full_train[!names(lgbti_full_train) %in% c("open_at_work_bin")]
 
-y <- lgbti_partial_train$open_at_work_bin
+y <- lgbti_full_train$open_at_work_bin
 
-predictor_logit <- Predictor$new(logit_train_output_partial, data = X, y = y)
+predictor_logit <- Predictor$new(logit_train_output_full, data = X, y = y)
 
-predictor_rf    <- Predictor$new(rf_train_output_partial, data = X, y = y)
+predictor_rf    <- Predictor$new(rf_train_output_full, data = X, y = y)
+
+#auc_error = function(actual, predicted) 1 - Metrics::auc(actual, predicted)
 
 # run the scores generation with parallelisation
 plan("callr", workers = 2)
 
 system.time(
-logit_full_featimp <- FeatureImp$new(predictor_logit, loss = "ce")
+logit_full_featimp_ce <- FeatureImp$new(predictor_logit, loss = "ce")
 )
 
 system.time(
-rf_full_featimp    <- FeatureImp$new(predictor_rf, loss = "ce")
+rf_full_featimp_ce    <- FeatureImp$new(predictor_rf, loss = "ce")
 )
 
-
+#plot(rf_full_featimp_auc)
 # save the feat imp objects of both models 
 
-save(logit_full_featimp,file = "./output/ml_object/caret/full/logit_full_featimp.RData")
-save(rf_full_featimp,file = "./output/ml_object/caret/full/rf_full_featimp.RData")
+save(logit_full_featimp_ce,file = "./output/ml_object/caret/full/logit_full_featimp_ce.RData")
+save(rf_full_featimp_ce,file = "./output/ml_object/caret/full/rf_full_featimp_ce.RData")
 
 
 
