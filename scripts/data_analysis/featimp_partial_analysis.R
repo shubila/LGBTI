6d1675da691e6b6f43a87092bef1448b56977368
shubila <shubi@hotmail.com>
2021-05-09 22:02:31 +0200

    Updated featimp script

--- scripts/data_analysis/featimp_partial_analysis.R
+++ scripts/data_analysis/featimp_partial_analysis.R
@@ -1,25 +1,26 @@
 
 library(tidyverse)
+library(caret)
 
 #clear the global environment
 rm(list = ls(all.names = TRUE))
 
 # load partial featimp objects
 
-load("./output/ml_object/caret/partial/logit_partial_featimp_ce.RData")
-load("./output/ml_object/caret/partial/rf_partial_featimp_ce.RData")
+
+load("./output/ml_object/caret/partial/rf_partial_model.RData")
 source("./scripts/data_manipulation/import_rename_predictors.R")
 
 
 # extract the results table from the featimp object
 
-logit_featimp_partial       <- logit_partial_featimp_ce$results
 
-rf_featimp_partial          <- rf_partial_featimp_ce$results 
+rf_featimp_partial          <- varImp(rf_train_output_partial_v2)$importance 
+rf_featimp_partial          <- rownames_to_column(rf_featimp_partial, var = "feature")%>%
+                                                arrange(., desc(Overall))
 
 # join the new names of predictors
 
-logit_featimp_partial <- inner_join(logit_featimp_partial,rename_pred, by = c("feature" = "old"))
 rf_featimp_partial    <- inner_join(rf_featimp_partial,rename_pred,    by = c("feature" = "old"))
 
 # how many variables to include in the plot 
@@ -27,31 +28,17 @@ rf_featimp_partial    <- inner_join(rf_featimp_partial,rename_pred,    by = c("f
 number_vars <- 30
 
 
-# generate logit plot 
-
-plot_logit_featimp <- logit_featimp_partial %>%
-  slice_max(importance, n = number_vars) %>%
-  ggplot(., aes(importance, reorder(new, importance))) +
-  geom_point(size = 2) +
-  geom_linerange(aes(xmin = importance.05, xmax = importance.95)) +
-  ggtitle("logit") +
-  ylab("feature") +
-  theme(
-        axis.text.y = element_text(size = 8, face = "bold"),
-        plot.title = element_text(size = 10)
-        )
-
-
 
 # generate rf plot 
 
 plot_rf_featimp <- rf_featimp_partial %>%
-  slice_max(importance, n = number_vars) %>%
-  ggplot(., aes(importance, reorder(new, importance))) +
+  slice_max(Overall, n = number_vars) %>%
+  ggplot(., aes(Overall, reorder(new, Overall))) +
   geom_point(size = 2) +
-  geom_linerange(aes(xmin = importance.05, xmax = importance.95)) +
+  #geom_linerange(aes(xmin = importance.05, xmax = importance.95)) +
   ggtitle("randomforest") +
   ylab("feature") +
+  xlab("importance") +
   theme(
     axis.text.y = element_text(size = 8, face = "bold"),
     plot.title = element_text(size = 10)
@@ -59,7 +46,7 @@ plot_rf_featimp <- rf_featimp_partial %>%
 
 
 
-(plot_featimp_ce <- gridExtra::grid.arrange(plot_logit_featimp, plot_rf_featimp, nrow = 1))
+ plot_rf_featimp
 
 
 
